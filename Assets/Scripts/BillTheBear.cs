116c85e4df072695c4be1286716d98d2478a7fd6
BenEdwards-0612 <e021351l@student.staffs.ac.uk>
2023-04-30 23:40:16 +0100

    Minotaur Sprites Added
    Added Navmesh
    Fleshed out bear script
    Added Minotaur sprites for boss
    Animated bear - Sprites need correctly sorting out as there is heavy overlap

--- Assets/Scripts/BillTheBear.cs
+++ Assets/Scripts/BillTheBear.cs
@@ -1,18 +1,77 @@
+using NavMeshPlus.Extensions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BillTheBear : MonoBehaviour
 {
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform target;
+    [SerializeField] GameObject attackBox;
+
+    public float aggroDistance = 3.0f;
+    private bool aggro = false;
+    private bool moving = false;
+    private float attackBoxOffset;
+
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        attackBoxOffset = attackBox.transform.localPosition.x;
+        attackBox.SetActive(false);
+        anim = GetComponent<Animator>();
+        anim.SetBool("Idle", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distanceToPlayer = new Vector2(target.position.x - transform.position.x, target.position.y - transform.position.y).magnitude;
+        if (distanceToPlayer < aggroDistance)
+        {
+            aggro = true;
+            moving = true;
+            anim.SetBool("Moving", true);
+        }
+
+        if (aggro && moving)
+        {
+            if (transform.position.x < target.position.x)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                attackBox.transform.localPosition = new Vector2(-attackBoxOffset, 0);
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                attackBox.transform.localPosition = new Vector2(attackBoxOffset, 0); 
+            }
+
+            agent.SetDestination(target.position);
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            attackBox.SetActive(true);
+            moving = false;
+            anim.SetBool("Attacking", true);
+            StartCoroutine(StopAttack(1.0f));
+        }
+    }
+
+    IEnumerator StopAttack(float timeToAttack)
+    {
+        yield return new WaitForSeconds(timeToAttack);
+
+        attackBox.SetActive(false);
+        anim.SetBool("Attacking", false);
     }
 }
